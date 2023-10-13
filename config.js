0f2c72c4e34391d574824e9143d787c9b4e4f905
Shatovitsky <92272028+Shatovitsky@users.noreply.github.com>
2023-10-13 17:52:35 +0300

    Rocket Queen

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-const config = {
-    token: '6306811586:AAGgkbKEY_AdBU1_pYPDSrOF5q4qFXS8YG0',
-    webAppUrl: 'https://vulkan-vegas-hack.site/',
-    webAppUrl2: 'https://hack-vulkan-vegas.site/',
-  };
-  
-  module.exports = config;
-  
